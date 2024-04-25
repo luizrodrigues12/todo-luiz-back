538303338a88ca2082f373bca8843c069912ec05
luizrodrigues12 <luizrodrigues2021@icloud.com>
2024-04-25 09:06:50 -0300

    Connect to atlas database

--- src/instances/mongo.ts
+++ src/instances/mongo.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
-const connect = mongoose.connect("mongodb://localhost:27017/to-do");
+const connect = mongoose.connect(
+  "mongodb+srv://lort:<Cleibson123>@nodeexpressprojects.ehv28f7.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProjects"
+);
 
 export default connect;
