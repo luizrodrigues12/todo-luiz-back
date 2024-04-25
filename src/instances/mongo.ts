9d3843e5c45a380947d365f3bd875f516bddb4be
luizrodrigues12 <luizrodrigues2021@icloud.com>
2024-04-25 15:13:27 -0300

    Secret

--- src/instances/mongo.ts
+++ src/instances/mongo.ts
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connect = mongoose.connect(
-  "mongodb+srv://lort:Cleibson123@nodeexpressprojects.ehv28f7.mongodb.net/to-do?retryWrites=true&w=majority&appName=NodeExpressProjects"
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@nodeexpressprojects.ehv28f7.mongodb.net/to-do?retryWrites=true&w=majority&appName=NodeExpressProjects`
 );
 
 export default connect;
