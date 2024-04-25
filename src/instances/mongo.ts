a1a255f1e566fcb382ee8eb9e947f2ca2b615352
luizrodrigues12 <luizrodrigues2021@icloud.com>
2024-04-25 10:49:14 -0300

    Fix password error on atlas link

--- src/instances/mongo.ts
+++ src/instances/mongo.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connect = mongoose.connect(
-  "mongodb+srv://lort:<Cleibson123>@nodeexpressprojects.ehv28f7.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProjects"
+  "mongodb+srv://lort:Cleibson123@nodeexpressprojects.ehv28f7.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProjects"
 );
 
 export default connect;
